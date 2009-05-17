07d0f3e832864495e620055cf983b4641c958dcd
chrisrhoden <carhoden@gmail.com>
2009-05-17 14:00:43 -0400

    adding taggability to post There are some problems with the DataMapper many-to-many implementation and as a result, I had to throw together this little hack.

--- models/post.rb
+++ models/post.rb
@@ -1,9 +1,9 @@
 class Post
   property :id, Serial
-  property :title, String
+  property :title, String, :nullable => false
   property :slug, String
   has n, :folksonomies
-  has n, :tags, :through => :folksonomies
+  has n, :tags, :through => :folksonomies, :mutable => true
   has n, :footprints
   
   before :save, :sluggify
@@ -13,8 +13,26 @@ class Post
         slug.downcase.gsub(/ /, '_').gsub(/[^a-z0-9_]/, '').squeeze('_')
   end
   
+  
+  # add_tag and remove_tag are hacky, and shouldn't be necessary, but
+  # the datamapper many-to-many implementation appears to be a bit
+  # screwy at the moment, so we'll go with this.
+  def add_tag(tag)
+    folksonomies.build(:tag_id => (tag = Tag.create(:name => tag.to_s)).id)
+    save; tags.reload
+    tag
+  end
+  
+  def remove_tag(tag)
+    r = Folksonomy.first :post_id => @id,
+        :tag_id => (t = Tag.first(:name=>tag.to_s)).id
+    r.destroy unless r.nil?; t.posts.reload unless t.nil?
+    tags.reload
+  end
+  
   def sluggify
     self.slug= @title if slug.nil? or slug == ''
   end
+
   
 end
--- test/post.rb
+++ test/post.rb
@@ -1,38 +1,51 @@
 require 'test/base'
 describe Post do
   
-  @post = Post.new
 
   it "can be created as an empty Post Object" do
-    @post.title.should.be.nil
-    @post.tags.should.equal []
-    @post.slug.should.be.nil
-    @post.footprints.should.equal []
+    p = Post.new
+    p.title.should.be.nil
+    p.tags.should.equal []
+    p.slug.should.be.nil
+    p.footprints.should.equal []
   end
 
   it "can have it's attributes updated and read" do
-    @post.title = 'a testing post'
-    @post.slug = 'test'
-    @post.title.should.equal 'a testing post'
-    @post.slug.should.equal 'test'
+    p = Post.new
+    p.title = 'a testing post'
+    p.slug = 'test'
+    p.title.should.equal 'a testing post'
+    p.slug.should.equal 'test'
   end
 
   it "can be saved and read from the database" do
-    @post.save
-    Post.get(@post.id).should.equal @post
+    p = Post.new
+    p.save.should.equal false
+    p.title = 'a'
+    p.save
+    Post.get(p.id).should.equal p
   end
   
   it "handles sluggifys on setting slug" do
-    @post.slug = 'My Little Sample Slug + All  that J4zz'
-    @post.slug.should.equal 'my_little_sample_slug_all_that_j4zz'
+    p = Post.new
+    p.slug = 'My Little Sample Slug + All  that J4zz'
+    p.slug.should.equal 'my_little_sample_slug_all_that_j4zz'
   end
   
   it "automatically creates a slug from title" do
-    @post = Post.new
-    @post.title = 'A Test Post'
-    @post.save
-    @post.slug.should.equal 'a_test_post'
-    Post.get(@post.id).slug.should.equal 'a_test_post'
+    p = Post.new
+    p.title = 'A Test Post'
+    p.save
+    p.slug.should.equal 'a_test_post'
+    Post.get(p.id).slug.should.equal 'a_test_post'
+  end
+  
+  it "allows adding and removing tags" do
+    p = Post.new(:title => 'a')
+    p.add_tag('ducky')
+    p.tags.first.name.should.equal 'ducky'
+    p.remove_tag('ducky')
+    p.tags.should.equal []
   end
   
 end
